d7568a1652c7eea7a60d8199a4f33814db7d6d0c
Jan Golda <jan.golda@gmail.com>
2022-04-02 23:22:17 +0200

    add 'TrafficNode'

---
+++ src/sim/TrafficNode.cpp
@@ -0,0 +1,35 @@
+#include "TrafficNode.h"
+
+using namespace sim;
+
+int TrafficNode::getLimit() const {
+    return queueLimit;
+}
+
+void TrafficNode::setLimit(int limit) {
+    queueLimit = limit;
+}
+
+int TrafficNode::getSize() const {
+    return (int) queue.size();
+}
+
+bool TrafficNode::isFull() const {
+    return getSize() >= getLimit();
+}
+
+bool TrafficNode::push(TrafficEntity *entity) {
+    if (isFull()) return false;
+    queue.push(entity);
+    return true;
+}
+
+TrafficEntity *TrafficNode::pop() {
+    TrafficEntity *e = queue.front();
+    queue.pop();
+    return e;
+}
+
+TrafficEntity *TrafficNode::peek() const {
+    return queue.front();
+}
---
+++ src/sim/TrafficNode.h
@@ -0,0 +1,34 @@
+#ifndef PW_TRAFFICSIMULATION_TRAFFICNODE_H
+#define PW_TRAFFICSIMULATION_TRAFFICNODE_H
+
+#include <glm/vec2.hpp>
+#include <queue>
+#include "TrafficEntity.h"
+
+namespace sim {
+    class TrafficNode {
+    private:
+        int queueLimit = 0;
+        std::queue<TrafficEntity *> queue;
+
+    public:
+        glm::vec2 position;
+
+        void setLimit(int limit);
+
+        [[nodiscard]] int getLimit() const;
+
+        [[nodiscard]] int getSize() const;
+
+        [[nodiscard]] bool isFull() const;
+
+        bool push(TrafficEntity *entity);
+
+        TrafficEntity *pop();
+
+        [[nodiscard]] TrafficEntity *peek() const;
+    };
+}
+
+
+#endif //PW_TRAFFICSIMULATION_TRAFFICNODE_H
