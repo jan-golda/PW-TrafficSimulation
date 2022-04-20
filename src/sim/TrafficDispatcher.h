d5fe91630836fcab6262a9a06135229fabf412f5
Jan Golda <jan.golda@gmail.com>
2022-04-20 11:13:52 +0200

    cleanup `TrafficDispatcher`

--- src/sim/TrafficDispatcher.h
+++ src/sim/TrafficDispatcher.h
@@ -1,10 +1,20 @@
 #ifndef PW_TRAFFICSIMULATION_TRAFFICDISPATCHER_H
 #define PW_TRAFFICSIMULATION_TRAFFICDISPATCHER_H
 
+
 namespace sim {
+
+    /**
+     * Abstract base class for traffic dispatchers that manage entities within their area of responsibility.
+     */
     class TrafficDispatcher {
     public:
+        /**
+         * Updates the dispatcher by performing one step of its logic.
+         * @param elapsed Time (in seconds) from the last logic update.
+         */
         virtual void update(float elapsed) = 0;
+
     };
 }
 
