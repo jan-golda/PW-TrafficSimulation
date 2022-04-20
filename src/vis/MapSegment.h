48160fbb2d463c12d3fbd18c12cfebc9ae2a7280
Jan Golda <jan.golda@gmail.com>
2022-04-20 12:10:36 +0200

    add `MapSegment` interface

---
+++ src/vis/MapSegment.h
@@ -0,0 +1,16 @@
+#ifndef PW_TRAFFICSIMULATION_MAPSEGMENT_H
+#define PW_TRAFFICSIMULATION_MAPSEGMENT_H
+
+
+#include <SFML/Graphics.hpp>
+#include "../sim/TrafficDispatcher.h"
+
+namespace vis {
+    /**
+     * Interface of a part of road infrastructure.
+     */
+    class MapSegment : public sf::Drawable {};
+}
+
+
+#endif //PW_TRAFFICSIMULATION_MAPSEGMENT_H
