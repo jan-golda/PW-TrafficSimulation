f327e9846736eb2d8fd9e8003b9e029a00b4525a
Jan Golda <jan.golda@gmail.com>
2022-04-21 04:36:46 +0200

    tweak default window size

--- src/vis/Window.cpp
+++ src/vis/Window.cpp
@@ -7,7 +7,7 @@ Window::Window() {
     sf::ContextSettings settings;
     settings.antialiasingLevel = 8;
 
-    window.create(sf::VideoMode(800, 600), "PW-TrafficSimulation", sf::Style::Default, settings);
+    window.create(sf::VideoMode(750, 600), "PW-TrafficSimulation", sf::Style::Default, settings);
     window.setFramerateLimit(60);
 }
 
